f0ddf52ee789b8e446e70057c853e027fdc4bdae
Alex Shelkovskiy <alexshelkov@gmail.com>
2025-03-14 17:56:54 +0100

    Fixed dotenv

--- src/env.ts
+++ src/env.ts
@@ -1,4 +1,3 @@
-import 'dotenv/config';
-
-export const baseURL = process.env.BASE_URL || 'eu-west-1.api.neuphonic.com';
-export const apiKey = process.env.API_KEY!;
+export const baseURL =
+  process.env.NEUPHONIC_BASE_URL || 'eu-west-1.api.neuphonic.com';
+export const apiKey = process.env.NEUPHONIC_API_KEY!;
