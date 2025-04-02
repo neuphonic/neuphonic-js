40cf06396345f2bfd059134240d98d923a68118b
Alex Shelkovskiy <alexshelkov@gmail.com>
2025-04-02 17:36:23 +0200

    Fix exports

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,3 @@ export type { TtsConfig, TtsMessage } from './common';
 export { WsErr } from './common';
 export type { Tts, Socket, Sse } from './tts';
 export type { Voice } from './voices';
-export type { RestoreJob, RestoreJobStatus } from './restorations';
--- src/indexBrowser.ts
+++ src/indexBrowser.ts
@@ -4,4 +4,3 @@ export type { TtsConfig } from './common';
 export { WsErr } from './common';
 export type { BrowserTts as Tts, Player, Socket, Sse } from './tts';
 export type { Voice } from './voices';
-export type { RestoreJob, RestoreJobStatus } from './restorations';
